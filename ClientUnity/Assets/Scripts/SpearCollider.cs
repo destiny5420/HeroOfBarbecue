86fdc8d5dfffa01022ac002abb116e86a36f2533
JohnnyLiu <vity255132@gmail.com>
2018-08-25 18:53:44 +0800

    Johnnyliu Add Spear Script

---
+++ ClientUnity/Assets/Scripts/SpearCollider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearCollider : MonoBehaviour {
+
+	public List<string> HitObj;
+	public GameObject[] HitPoint1;
+	public GameObject[] HitPoint2;
+	public GameObject[] HitPoint3;
+
+	void Update()
+	{
+		for (int i = 0; i < 5; i++) 
+		{
+			if (HitPoint1 [i].transform.name == HitObj [i]+" (Clone)" && HitObj.Count >0) 
+			{
+				HitPoint1 [i].SetActive (true);
+			}
+			if (HitPoint2 [i].transform.name == HitObj [i]+" (Clone)" && HitObj.Count >1) 
+			{
+				HitPoint2 [i].SetActive (true);
+			}
+			if (HitPoint3 [i].transform.name == HitObj [i]+" (Clone)" && HitObj.Count >2) 
+			{
+				HitPoint3 [i].SetActive (true);
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.transform.tag == "Food") 
+		{
+			HitObj.Add (collision.transform.name);
+		}
+	}
+}
