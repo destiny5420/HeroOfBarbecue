20ec5f1f1b85eba9b9b821c7abefbbe13efadf0e
Sikapaper_Mac <destiny5420@gmail.com>
2018-08-25 19:31:59 +0800

    [Update] Ready & Go text

--- ClientUnity/Assets/Scripts/Controller/PlayerUIController.cs
+++ ClientUnity/Assets/Scripts/Controller/PlayerUIController.cs
@@ -3,31 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerUIController : MonoBehaviour 
+public class PlayerUIController : MonoBehaviour
 {
     [SerializeField] Text m_txtTimer;
+    [SerializeField] Text m_txtMessage;
     [SerializeField] RectTransform m_rectTranWinnerPanel;
+    [SerializeField] RectTransform m_rectTranReadyPanel;
     [SerializeField] Transform m_tranShiny;
+    [SerializeField] Transform m_tranMessage;
+
     Vector3 m_v3WinnerPanelHidePos = new Vector3(0.0f, 2000.0f, 0.0f);
     Vector3 m_v3WinnerPanelShowPos = new Vector3(0.0f, 0.0f, 0.0f);
+
+    Vector3 m_v3ReadyPanelHidePos = new Vector3(0.0f, 3000.0f, 0.0f);
+    Vector3 m_v3ReadyPanelShowPos = new Vector3(0.0f, 0.0f, 0.0f);
+
+    Vector3 m_v3StartScale = new Vector3(3.0f, 3.0f, 3.0f);
+    Vector3 m_v3EndScale = new Vector3(1.0f, 1.0f, 1.0f);
 
     // Timer
     bool bRotShiny;
     const float m_fRoyShinyUnit = 100.0f;
+
+    bool bScale;
+    const float m_fDefaultScaleValue = 0.0f;
+    const float m_fScaleTime = 0.3f;
+    float m_fScaleValue;
+
+    bool bDelayGo;
+    const float m_fDefaultDelayGoClock = 0.0f;
+    const float m_fDelayGoTime = 1.0f;
+    float m_fDelayGoClock;
+
+    bool m_bStartGame;
 
     void Awake()
     {
         GameLogic.GetInstance().UIMediator.Regist_PlayerUIController(this);
     }
 
-    void Start ()
+    void Start()
     {
-        
-	}
+
+    }
 
     void Update()
     {
-        
+        if (bScale)
+        {
+            m_fScaleValue += Time.deltaTime / m_fScaleTime;
+            m_tranMessage.localScale = Vector3.Lerp(m_v3StartScale, m_v3EndScale, m_fScaleValue);
+
+            if (m_fScaleValue >= 1.0f)
+                OnScaleValueComplete();
+        }
+
+        if (bDelayGo)
+        {
+            m_fDelayGoClock += Time.deltaTime;
+            if (m_fDelayGoClock >= m_fDelayGoTime)
+                OnDelayGoComplete();
+        }
     }
 
     void LateUpdate()
@@ -40,11 +76,20 @@ public class PlayerUIController : MonoBehaviour
 
     public void Init()
     {
-        Debug.Log("PlayerUIController Init");
         bRotShiny = false;
         m_tranShiny.eulerAngles = Vector3.zero;
+        m_txtMessage.text = "";
         m_txtTimer.text = "";
         m_rectTranWinnerPanel.transform.position = m_v3WinnerPanelHidePos;
+        m_rectTranReadyPanel.transform.position = m_v3ReadyPanelHidePos;
+
+        bScale = false;
+        m_fScaleValue = m_fDefaultScaleValue;
+
+        bDelayGo = false;
+        m_fDelayGoClock = m_fDefaultDelayGoClock;
+
+        m_bStartGame = false;
     }
 
     public void SetTimer(float timer)
@@ -52,9 +97,62 @@ public class PlayerUIController : MonoBehaviour
         m_txtTimer.text = timer.ToString();
     }
 
+    public void Handle_Ready()
+    {
+        SettingMessage("READY");
+        bScale = true;
+        m_fScaleValue = m_fDefaultScaleValue;
+        m_tranMessage.localScale = Vector3.Lerp(m_v3StartScale, m_v3EndScale, m_fScaleValue);
+        m_rectTranReadyPanel.transform.position = m_v3ReadyPanelShowPos;
+    }
+
+    public void Handle_Go()
+    {
+        SettingMessage("GO");
+        bScale = true;
+        m_fScaleValue = m_fDefaultScaleValue;
+        m_tranMessage.localScale = Vector3.Lerp(m_v3StartScale, m_v3EndScale, m_fScaleValue);
+    }
+
+    void Handle_DelayGo()
+    {
+        m_bStartGame = true;
+        bDelayGo = true;
+        m_fDelayGoClock = m_fDefaultDelayGoClock;
+    }
+
     public void GameOver()
     {
         bRotShiny = true;
         m_rectTranWinnerPanel.transform.position = m_v3WinnerPanelShowPos;
     }
+
+    void SettingMessage(string message)
+    {
+        m_txtMessage.text = message;
+    }
+
+    #region Complete
+    void OnScaleValueComplete()
+    {
+        bScale = false;
+
+        m_fScaleValue = 1.0f;
+        m_tranMessage.localScale = Vector3.Lerp(m_v3StartScale, m_v3EndScale, m_fScaleValue);
+
+        if (m_bStartGame)
+        {
+            Debug.Log("Go");
+        }
+        else
+            Handle_DelayGo();
+    }
+
+    void OnDelayGoComplete()
+    {
+        bDelayGo = false;
+
+        Handle_Go();
+    }
+    #endregion
 }
