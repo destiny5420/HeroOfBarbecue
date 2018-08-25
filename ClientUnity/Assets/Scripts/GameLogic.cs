fbe92499debaa84ff34c6447163a4a4cc0cbf0f1
Sikapaper_Mac <destiny5420@gmail.com>
2018-08-25 12:41:44 +0800

    [Add] PlayerProxy / PlayerMediator / PlayerController [Modify] GameLogic

--- ClientUnity/Assets/Scripts/GameLogic.cs
+++ ClientUnity/Assets/Scripts/GameLogic.cs
@@ -9,11 +9,21 @@ public class GameLogic : MonoBehaviour
     static GameLogic m_instance;
     public static GameLogic GetInstance() { return m_instance; }
 
+    // Mediator
+    PlayerMediator m_PlayerMediator;
+
+    // Proxy
+    PlayerProxy m_PlayerProxy;
+
     void Awake()
     {
         Debug.LogWarning("GameLogic / Awake");
 
         m_instance = this;
+
+        m_PlayerMediator = new PlayerMediator();
+
+        m_PlayerProxy = new PlayerProxy();
 
         DontDestroyOnLoad(this);
     }
@@ -22,13 +32,16 @@ public class GameLogic : MonoBehaviour
     {
         Debug.LogWarning("GameLogic / Start");
 
+        m_PlayerMediator.Start();
+        m_PlayerProxy.Start();
 
         Init();
     }
 
     void Init()
     {
-
+        m_PlayerMediator.Init();
+        m_PlayerProxy.Init();
     }
 
     void Update()
@@ -37,6 +50,20 @@ public class GameLogic : MonoBehaviour
     }
 
     #region Proxy & Mediator
+    public PlayerMediator PlayerMediator
+    {
+        get {
+           return m_PlayerMediator;
+        }
+    }
+
+    public PlayerProxy PlayerProxy
+    {
+        get
+        {
+            return m_PlayerProxy;
+        }
+    }
 
     #endregion
 }
---
+++ ClientUnity/Assets/Scripts/Mediator/PlayerMediator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMediator 
+{
+    PlayerController m_clsPlayerController1;
+    PlayerController m_clsPlayerController2;
+
+    public void Start () 
+    {
+		
+	}
+	
+    public void Init()
+    {
+
+    }
+
+	void Update () 
+    {
+		
+	}
+
+    public void RegistPlayerController1(PlayerController r_playerController)
+    {
+        m_clsPlayerController1 = r_playerController;
+    }
+
+    public void RegistPlayerController2(PlayerController r_playerController)
+    {
+        m_clsPlayerController2 = r_playerController;
+    }
+}
