854060645c2e94ac3c0d0e3a478a5c02ac2237f2
Sikapaper_Mac <destiny5420@gmail.com>
2018-08-26 11:22:52 +0800

    update

--- ClientUnity/Assets/Scripts/Controller/CameraController.cs
+++ ClientUnity/Assets/Scripts/Controller/CameraController.cs
@@ -2,31 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour 
+public class CameraController : MonoBehaviour
 {
     [SerializeField] List<Transform> m_tranTarget;
     Vector3 m_v3Offset = new Vector3(0.0f, 35.0f, -35.0f);
 
-    [SerializeField] float m_fSmoothTime = 0.5f;
+    float m_fSmoothTime = 0.15f;
     Vector3 v3Velocity;
 
-    [SerializeField] float m_fMinZoom = 40.0f;
-    [SerializeField] float m_fMaxZoom = 20.0f;
-    [SerializeField] float m_fZoomLimit = 50.0f;
+    float m_fMinZoom = 20.0f;
+    float m_fMaxZoom = 10.0f;
+    float m_fZoomLimit = 50.0f;
+
     [SerializeField] Camera m_mainCamera;
 
-	void Start () 
-    {
+    // Shake
+    Vector3 axisShakeMin = new Vector3(1.0f, 1.0f, 1.0f);
+    Vector3 axisShakeMax = new Vector3(1.05f, 1.05f, 1.05f);
+    float timeOfShake;
+    float timeOfShakeStore;
+    bool shake;
+    Vector3 startPos;
 
-	}
-	
-    void Init()
+    void Start()
     {
-
+        Init();
     }
 
-	void Update () 
+    void Init()
     {
+        shake = false;
+        startPos = transform.position;
+        timeOfShakeStore = timeOfShake;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("Press Camera / B");
+            ShakeCamera(0.5f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (shake)
+        {
+            transform.position = startPos + new Vector3(Random.Range(axisShakeMin.x, axisShakeMax.x), Random.Range(axisShakeMin.y, axisShakeMax.y), Random.Range(axisShakeMin.z, axisShakeMax.z));
+            timeOfShake -= Time.deltaTime;
+            if (timeOfShake <= 0.0f)
+            {
+                shake = false;
+                transform.position = startPos;
+            }
+        }
     }
 
     void LateUpdate()
@@ -74,5 +104,19 @@ public class CameraController : MonoBehaviour
         }
 
         return bounds.center;
+    }
+
+    public void ShakeCamera(float shakeTime = -1.0f)
+    {
+        if (shakeTime > 0.0f)
+        {
+            timeOfShake = shakeTime;
+        }
+        else
+            timeOfShake = timeOfShakeStore;
+
+        shake = true;
+
+        startPos = transform.position;
     }
 }
