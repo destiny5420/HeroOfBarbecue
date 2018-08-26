f980cc10381cc03406a5b81e67a4ec550fe24ae7
JohnnyLiu <vity255132@gmail.com>
2018-08-26 11:05:13 +0800

    JohnnyLiu 重置食材

--- ClientUnity/Assets/Scripts/Mediator/FoodGeneratorNew.cs
+++ ClientUnity/Assets/Scripts/Mediator/FoodGeneratorNew.cs
@@ -19,7 +19,10 @@ public class FoodGeneratorNew : MonoBehaviour
 
 	void Update () 
 	{
-		
+		if (Input.GetKeyDown (KeyCode.F9)) 
+		{
+			Reset ();
+		}
 	}
 
 	public void Init()
@@ -43,5 +46,22 @@ public class FoodGeneratorNew : MonoBehaviour
 	{
 		FruitCreated.Remove (Fruit);
 		Destroy (Fruit);
+	}
+
+	public void Reset()
+	{
+		for (int i = 0; i < FruitCreated.Count; i++) 
+		{
+			Destroy (FruitCreated [i]);
+		}
+		FruitCreated.Clear ();
+		if (FruitCreated.Count < MaxCount) 
+		{
+			for (int i = 0; i < Random.Range (7, 10); i++) 
+			{
+				GameObject Fruits = Instantiate (FruitKind [Random.Range (0, FruitKind.Length)], new Vector3 (Random.Range (-10, 10), 20, Random.Range (-10, 10)), transform.rotation, FruitMother);
+				FruitCreated.Add (Fruits);
+			}
+		}
 	}
 }
