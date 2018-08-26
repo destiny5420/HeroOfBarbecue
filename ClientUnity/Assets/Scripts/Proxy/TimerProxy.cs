3aee71500642ab98c7e8b6f4c917836229d6b736
JohnnyLiu <vity255132@gmail.com>
2018-08-26 12:16:07 +0800

    JohnnyLiu 更新音效 魚貼圖

--- ClientUnity/Assets/Scripts/Proxy/TimerProxy.cs
+++ ClientUnity/Assets/Scripts/Proxy/TimerProxy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TimerProxy
 {
-    const float m_fDefaultGameBaseTime = 30.0f;
+    const float m_fDefaultGameBaseTime = 180.0f;
     const float m_fGameBaseTimeOfOver = 1.0f;
     bool m_bStart_GameBaseClock;
     float m_fGameBaseClock;
